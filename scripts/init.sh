71c262880f82bad700106dcb877ae1b46741898d
Ethan Harsh <ethan@ethanharsh.com>
2022-09-28 01:25:29 -0400

    remove init script

--- scripts/init.sh
+++
@@ -1,4 +0,0 @@
-npm init -y
-npm install express dotenv typescript
-npm install @types/jest @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-google eslint-config-prettier jest nodemon prettier ts-jest --save-dev
-mkdir src dist test
